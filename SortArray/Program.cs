09926ee0daff03489549f8c9c2b0118b5bcd0756
alejandi <alejotesa@hotmail.com>
2016-03-03 17:54:02 -0500

    Se addicionaron datos de prueba

--- SortArray/Program.cs
+++ SortArray/Program.cs
@@ -9,9 +9,12 @@ namespace SortArray
 {
     class Program
     {
-        static int[] a = new int[] { 5, 3, 4, 2, 1, 8, 7, 6 };
+        //static int[] a = new int[] { 5, 3, 4, 2, 1, 8, 7, 6 };
         //static int[] a = new int[] { 5, 3, 4, 2, 1, 6, 7, 8 };
         //static int[] a = new int[] { 5, 7, 8, 6, 1, 4, 3, 2 };
+        static int[] a = new int[] { 3, 8, 2, 6, 7, 1, 5, 4 };
+        //static int[] a = new int[] {1,3,4,5,2,6,7,8};
+        //static int[] a = new int[] { 2, 3, 7, 8, 1, 4, 5, 6 };
         static int n = a.Length;
         static int[] b = new int[n];
         static void Main(string[] args)
